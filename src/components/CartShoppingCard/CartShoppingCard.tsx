92abd1e3248ae2fe0856d0f62761ac860f691123
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2024-10-22 14:12:21 +0330

    working on UI and functionality of the cart page

---
+++ src/components/CartShoppingCard/CartShoppingCard.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {useDispatch} from "react-redux";
+import {addProduct, decrementProduct} from "../../store/states/productsSlice";
+
+interface ShoppingCartCardProps {
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+    quantity: number;
+}
+interface ProductDataTypes {
+    productData: ShoppingCartCardProps
+}
+
+const CartShoppingCard: React.FC<ProductDataTypes>  = ({productData} : ProductDataTypes) => {
+    const {id, name, price, image, quantity} = productData;
+    const dispatch = useDispatch();
+
+    const handleAddToCart = () => {
+        dispatch(addProduct({ id, name, price, image, quantity: 1 })); // مقدار اولیه quantity
+    };
+
+    const handleDecrement = () => {
+        dispatch(decrementProduct(id));
+    };
+
+    const totalPrice = price * quantity;
+    return (
+        <div className="flex justify-between items-center px-12 py-5 border-2 border-purple-700 rounded">
+            <div className="flex items-center justify-center gap-5">
+                <img src={image} alt="product img" className="h-28"/>
+                <div className="flex flex-col items-center text-center gap-3">
+                    <p className=" text-lg text-purple-700 font-semibold">
+                        {name}
+                    </p>
+                    <span className="text-gray-500 text-lg">
+                        {price.toLocaleString()} تومان
+                    </span>
+                </div>
+            </div>
+            <div className="flex flex-col items-center text-center gap-3">
+                <span className="text-lg text-fuchsia-500 font-semibold">
+                        {totalPrice.toLocaleString()} تومان
+                </span>
+                <div className="flex justify-center gap-5 items-center w-full h-11">
+                    <button
+                        className="bg-purple-700 text-white rounded p-4 h-11 flex justify-center items-center text-xl font-medium"
+                        onClick={handleAddToCart} // increase number of products
+                    >
+                        +
+                    </button>
+                    <span className="font-bold text-xl">{quantity}</span>
+                    <button
+                        className="border border-purple-700 text-purple-700 rounded p-4 h-11 flex justify-center items-center text-2xl font-medium"
+                        onClick={handleDecrement} // decrease number of products
+                    >
+                        -
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default CartShoppingCard;
--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,11 +1,43 @@
-// src/components/cart/Cart.tsx
 import React from 'react';
+import CartShoppingCard from "../../components/CartShoppingCard/CartShoppingCard";
+import {useSelector} from "react-redux";
+import {RootState} from "../../store/store";
 
 const Cart: React.FC = () => {
+    const products = useSelector((state: RootState) => state.products);
+
+    // Calculate the total number of selected products
+    const totalItems = products.reduce((total, product) => total + product.quantity, 0);
+
+    // Calculate the total price of selected products (price * quantity)
+    const totalPrice = products.reduce((total, product) => total + (product.price * product.quantity), 0);
+
     return (
-        <div>
-            <h2 className="text-2xl font-bold">سبد خرید</h2>
-            {/* Add Cart items here */}
+        <div className="flex flex-col justify-between min-h-[calc(75vh-4rem)]">
+            {/* Product list */}
+            <div className="grow">
+                {products.map((product, index) => (
+                    <div key={index} className="mb-5">
+                        <CartShoppingCard productData={product} />
+                    </div>
+                ))}
+            </div>
+
+            {/* Footer with total items and price */}
+            <div className="flex justify-between items-center p-4 bg-gray-50 shadow-lg mt-4">
+                <div className="flex flex-col gap-2">
+                    <p className="text-md text-fuchsia-500 font-semibold">
+                        تکمیل و ادامه فرآیند پرداخت {totalItems} کالا از {products.length} محصول انتخاب شده
+                    </p>
+                    <p className="text-lg text-purple-700 font-semibold">
+                        مبلغ پرداخت سفارش {totalPrice.toLocaleString()} تومان
+                    </p>
+                </div>
+                <button className="flex items-center gap-5 px-3.5 py-2.5 bg-purple-700 text-white rounded text-sm">
+                    تکمیل و پرداخت
+                    <img src={"/icons/WalletIcon.svg"} alt="wallet icon" className="h-8 w-8" />
+                </button>
+            </div>
         </div>
     );
 };
